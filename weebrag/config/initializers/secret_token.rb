c239d8e4f4b1ef621c1832083ca6621450a246e1
Joey Kirk <joey.kirk@gmail.com>
2012-10-12 16:31:32 -0500

    Deleted files

--- weebrag/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Weebrag::Application.config.secret_token = '6521ec3bc482b53444217983d64676a73a69901b7c23fbe47f60adfe493e011c0c3924cff4da4786ddd89e1b35a27fc211489acd7c4e3638d78ca97157213486'
